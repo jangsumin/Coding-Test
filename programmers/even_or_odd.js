99ab4e97ef7fd2d0398aeae47186b8f0f57a9105
jangsumin <jsm0726@naver.com>
2022-10-03 22:23:07 +0900

    complete evenOrodd problem

---
+++ programmers/even_or_odd.js
@@ -0,0 +1,18 @@
+function evenOrodd(num) {
+    return num % 2 ? "Odd" : "Even";
+}
+
+/*
+function evenOrodd(num) {
+    var answer = '';
+    if (num % 2 === 0) {
+        answer = "Even";
+    } else {
+        answer = "Odd";
+    }
+
+    return answer;
+}
+*/
+
+console.log(evenOrodd(2));
