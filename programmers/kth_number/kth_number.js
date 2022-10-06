d7a112f6628b972c70c905938638f60566ac6b4a
jangsumin <jsm0726@naver.com>
2022-10-06 12:39:37 +0900

    solve kth_number problem

---
+++ programmers/kth_number/kth_number.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+    var answer = [];
+
+    for (let i=0;i<commands.length;i++) {
+        let temp = array.slice(commands[i][0] - 1, commands[i][1]);
+        temp.sort(function(a, b) {
+            return a - b;
+        });
+        answer.push(temp[commands[i][2] - 1]);
+    }
+
+    return answer;
+}
