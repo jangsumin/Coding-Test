e6c600d17a7265f48bd506fa1b41d4e5aca97f7e
jangsumin <jsm0726@naver.com>
2022-11-04 16:04:20 +0900

    solve the 직각삼각형 출력하기 problem in programmers

---
+++ programmers/Lv0/직각삼각형 출력하기/solution.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+let str = '';
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    for (let i=0; i<Number(input[0]); i++) {
+        str += "*"
+        console.log(str);
+    }
+});
