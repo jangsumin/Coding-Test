62759d6bcc1f5d8ce0b6475d0c264455c3dd6670
jangsumin <jsm0726@naver.com>
2022-12-01 09:29:07 +0900

    solve the 배열 회전시키기 problem in programmers

---
+++ programmers/Lv0/배열 회전시키기/solution.js
@@ -0,0 +1,5 @@
+function solution(numbers, direction) {
+  if (direction === "right") numbers.unshift(numbers.pop());
+  if (direction === "left") numbers.push(numbers.shift());
+  return numbers;
+}
